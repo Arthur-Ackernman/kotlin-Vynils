fb2faaf25d2b45e419117220f6109d920c4ef59f
arthur-ackernman <ca.castrov12@uniandes.edu.co>
2022-11-02 21:02:19 -0500

    estructura carpetas

---
+++ app/src/main/java/com/moviles/vynils/models/Album.kt
@@ -0,0 +1,11 @@
+package com.moviles.vynils.models
+
+data class Album(
+    val albumId:Int,
+    val name:String,
+    val cover:String,
+    val releaseDate:String,
+    val description:String,
+    val genre:String,
+    val recordLabel:String
+)
