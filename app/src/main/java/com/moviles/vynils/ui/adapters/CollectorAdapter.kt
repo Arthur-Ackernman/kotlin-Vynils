15bb56fcea1b3f39d5b94346f2f394cfb3001b4b
arthur-ackernman <ca.castrov12@uniandes.edu.co>
2022-12-05 00:16:10 -0500

    fix collector

--- app/src/main/java/com/moviles/vynils/ui/adapters/CollectorAdapter.kt
+++ app/src/main/java/com/moviles/vynils/ui/adapters/CollectorAdapter.kt
@@ -4,10 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.moviles.vynils.R
 import com.moviles.vynils.databinding.CollectorItemBinding
 import com.moviles.vynils.models.Collector
+import com.moviles.vynils.ui.CollectorFragmentDirections
 
 
 class CollectorAdapter: RecyclerView.Adapter<CollectorAdapter.CollectorViewHolder>() {
@@ -29,6 +31,11 @@ class CollectorAdapter: RecyclerView.Adapter<CollectorAdapter.CollectorViewHolde
     override fun onBindViewHolder(holder: CollectorAdapter.CollectorViewHolder, position: Int) {
         holder.viewDataBinding.also {
             it.collector = collectors[position]
+        }
+        holder.viewDataBinding.root.setOnClickListener {
+            val action = CollectorFragmentDirections.actionCollectorFragmentToDetailCollectorFragment(collectors[position].collectorId)
+            // Navigate using that action
+            holder.viewDataBinding.root.findNavController().navigate(action)
         }
     }
 
